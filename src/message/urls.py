33464dd831ce0bbb710da5ce7b1c628671960ff7
ekrap <ptrece8@gmail.com>
2024-09-14 19:07:31 +0000

    route-message

--- src/message/urls.py
+++ src/message/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path, include
+from message.views import message_list
 
 
 
 urlpatterns = [
+
+     path('messages/',include([
+            path('<int:sujet_id>/list',message_list),
+        ])),
+
 ]
