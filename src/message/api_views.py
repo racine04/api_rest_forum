2fe745a7de20352f37f5cfdedccd5167a8b33514
ekrap <ptrece8@gmail.com>
2024-09-15 18:02:05 +0000

    modification nom du fichier views dans message

--- src/message/api_views.py
+++ src/message/api_views.py
@@ -1,27 +1,24 @@
-from django.http import HttpResponse, JsonResponse
+from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
-from message.models import MessageModel
-from message.serializers import MessageSerializer
+from .models import MessageModel
+from .serializers import MessageSerializer
 from sujet.models import SujetModel
 
 
-
 @csrf_exempt
-def message_list(request,sujet_id):
-
+def message_list(request, subject_id):
     if request.method == 'GET':
-        messages = MessageModel.objects.filter(subject=sujet_id)
-        serializer = MessageSerializer(messages, many = True)
+        messages = MessageModel.objects.filter(subject=subject_id)
+        serializer = MessageSerializer(messages, many=True)
         return JsonResponse(serializer.data, safe=False)
-    
+
     elif request.method == 'POST':
         data = JSONParser().parse(request)
         serializer = MessageSerializer(data=data)
-        subject = get_object_or_404(SujetModel, id = sujet_id)
+        subject = get_object_or_404(SujetModel, id=subject_id)
         if serializer.is_valid():
             serializer.save(subject=subject)
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
-
