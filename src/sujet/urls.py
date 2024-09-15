3192067f5c165cc678f34f5d38c1e7b7f28a0eb7
KevinArnaud32 <arnaudkevin32@gmail.com>
2024-09-15 18:22:54 +0000

    update subject

--- src/sujet/urls.py
+++ src/sujet/urls.py
@@ -1,16 +1,10 @@
-from django.urls import path, include
-from rest_framework import routers
+from django.urls import path
 from .api_view import sujet_list, sujet_detail
 
 
 app_name = 'api_sujet'
 
-router = routers.DefaultRouter()
-router.register(r'sujets', SujetViewset)
-
-
 urlpatterns = [
-    path('', include(router.urls)),
-    path('subjects/', sujet_list),
-    path('subjects/<int:pk>/', sujet_detail),
+    path('subject/<int:forum_id>/forum', sujet_list),
+    path('subject/<int:pk>/detail', sujet_detail),
 ]
